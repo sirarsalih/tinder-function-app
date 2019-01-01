fdd0e1abc387c5d0fc3c33b6d4de81a231fdc7b9
sirar87 <sirars@gmail.com>
2019-01-01 14:02:55 +0100

    Beautify the code. Handle recs timeout and increase duration to 15 minutes to avoid timeout.

--- TinderFunctionApp/LikeFunction.cs
+++ TinderFunctionApp/LikeFunction.cs
@@ -15,20 +15,17 @@ namespace TinderFunctionApp
     public static class LikeFunction
     {
         [FunctionName("LikeFunction")]
-        public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter log, ExecutionContext context)
+        public static async Task Run([TimerTrigger("0 */15 * * * *")]TimerInfo myTimer, TraceWriter log, ExecutionContext context)
         {
-            using (var client = new HttpClient())
-            {
-                try
-                {
+            using (var client = new HttpClient()) {
+                try {
                     var config = new ConfigurationBuilder()
                      .SetBasePath(context.FunctionAppDirectory)
                      .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
                      .AddEnvironmentVariables()
                      .Build();
                     var response = await client.PostAsJsonAsync("https://api.gotinder.com/auth", new Auth() { facebook_id = config["FacebookId"], facebook_token = config["FacebookToken"]});
-                    switch (response.StatusCode)
-                    {
+                    switch (response.StatusCode) {
                         case HttpStatusCode.OK:
                             log.Info($"Successful authentication. {(int)response.StatusCode} {response.ReasonPhrase}.");
                             var responseBody = await response.Content.ReadAsStringAsync();
@@ -36,27 +33,27 @@ namespace TinderFunctionApp
                             client.DefaultRequestHeaders.Add("X-Auth-Token", tinderToken);
                             var recs = await client.GetAsync("https://api.gotinder.com/user/recs");
                             var recsBody = await recs.Content.ReadAsStringAsync();
-                            var resultsJson = JToken.Parse(recsBody).Last().ToString();
-                            var encloseResultsJson = "{" + resultsJson + "}";
-                            var ms = new MemoryStream(System.Text.Encoding.ASCII.GetBytes(encloseResultsJson)) { Position = 0 };
-                            var ser = new DataContractJsonSerializer(typeof(Results));
-                            var results = (Results)ser.ReadObject(ms);
-                            foreach (var result in results.results)
-                            {
-                                var like = await client.GetAsync("https://api.gotinder.com/like/" + result._id);
-                                if (like.StatusCode == HttpStatusCode.OK)
-                                {
-                                    log.Info($"Successfully liked {result.name} who is {result.distance_mi} Miles away from my current location.");
+                            if(recsBody.Contains("recs timeout")) {
+                                log.Info($"Too many queries for new users in a too short period of time. Try again later.");
+                            } else {
+                                var resultsJson = JToken.Parse(recsBody).Last().ToString();
+                                var encloseResultsJson = "{" + resultsJson + "}";
+                                var ms = new MemoryStream(System.Text.Encoding.ASCII.GetBytes(encloseResultsJson)) { Position = 0 };
+                                var ser = new DataContractJsonSerializer(typeof(Results));
+                                var results = (Results)ser.ReadObject(ms);
+                                foreach (var result in results.results) {
+                                    var like = await client.GetAsync("https://api.gotinder.com/like/" + result._id);
+                                    if (like.StatusCode == HttpStatusCode.OK) {
+                                        log.Info($"Successfully liked {result.name} who is {result.distance_mi} Miles away from my current location.");
+                                    }
                                 }
-                            }
+                            }                            
                             break;
                         case HttpStatusCode.Unauthorized:
                             log.Info($"Unsuccessful authentication, check Facebook token. {(int)response.StatusCode} {response.ReasonPhrase}.");
                             break;                       
                     }
-                }
-                catch (HttpRequestException e)
-                {
+                } catch (HttpRequestException e) {
                     log.Error($"Exception caught! Message :{e.Message}.");
                 }
             }
